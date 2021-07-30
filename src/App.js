ba726d27c44c03659878e421be232e9311a1a793
Arjun more <amore@chirpn.com>
2021-07-30 16:02:32 +0530

    basic filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import ReactDataGrid from "react-data-grid";
+import {Data, Toolbar} from "react-data-grid-addons";
+import createRowData from "./createRowData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const defaultColumnProperties = {
+    filterable: true,
+    width: 120
+};
+
+const selectors = Data.Selectors;
+
+const columns = [
+    {
+        key: "id",
+        name: "ID"
+    },
+    {
+        key: "title",
+        name: "Title"
+    },
+    {
+        key: "firstName",
+        name: "First Name"
+    },
+    {
+        key: "lastName",
+        name: "Last Name"
+    },
+    {
+        key: "email",
+        name: "Email"
+    },
+    {
+        key: "street",
+        name: "Street"
+    },
+    {
+        key: "zipCode",
+        name: "ZipCode"
+    },
+    {
+        key: "date",
+        name: "Date"
+    },
+    {
+        key: "jobTitle",
+        name: "Job Title"
+    },
+    {
+        key: "catchPhrase",
+        name: "Catch Phrase"
+    },
+    {
+        key: "jobArea",
+        name: "Job Area"
+    },
+    {
+        key: "jobType",
+        name: "Job Type"
+    }
+].map(c => ({...c, ...defaultColumnProperties}));
+
+const ROW_COUNT = 50;
+
+const handleFilterChange = filter => filters => {
+    const newFilters = {...filters};
+    console.log('---')
+    console.log(filters)
+    console.log(filter)
+    if (filter.filterTerm) {
+        newFilters[filter.column.key] = filter;
+    } else {
+        delete newFilters[filter.column.key];
+    }
+    return newFilters;
+};
+
+function getRows(rows, filters) {
+    return selectors.getRows({rows, filters});
 }
 
-export default App;
+const rows = createRowData(10);
+
+export default function App() {
+    const [filters, setFilters] = useState({});
+    const filteredRows = getRows(rows, filters);
+    return (
+        <ReactDataGrid
+            columns={columns}
+            rowGetter={i => filteredRows[i]}
+            rowsCount={filteredRows.length}
+            minHeight={500}
+            toolbar={<Toolbar
+                filterRowsButtonText={"Filter"}
+                enableFilter={true}/>}
+            onAddFilter={filter => setFilters(handleFilterChange(filter))}
+            onClearFilters={() => setFilters({})}
+        />
+    );
+}
